b3106144e77c95e465ced928a98cb2fb00501189
Finn Schwoerer <finnschwoerer@Finns-MacBook-Pro.local>
2024-01-15 14:02:50 +0100

    Database now loads automatically when loginPage is called

--- src/main/java/com/example/objectorientation/controller/LogInController.java
+++ src/main/java/com/example/objectorientation/controller/LogInController.java
@@ -1,30 +1,65 @@
 package com.example.objectorientation.controller;
 
 import com.example.objectorientation.HelloApplication;
+import com.example.objectorientation.PathHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
 
 
 import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
 
-public class LogInController {
+public class LogInController implements Initializable{
 
     public LogInController(){
 
     }
+
+    private String jdbcURL = "jdbc:postgresql://localhost:5432/postgres";
+    private String user = "admin";
+    private String password = "password";
+    private String query = "";
 
     @FXML
     private Button loginButton;
     @FXML
     private Label errorLabel;
     @FXML
-    private TextField username;
+    private TextField usernameField;
     @FXML
-    private PasswordField password;
+    private PasswordField passwordField;
+
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1)
+    {
+        //init DB
+        try (Connection connection = DriverManager.getConnection(jdbcURL, user, password)) {
+            HelloController.class.getClassLoader().getResourceAsStream("schema.sql");
+            String filePath = PathHandler.relativePath("src main resources schema.sql");
+            Path of = Path.of(filePath);
+            query = Files.readString(of);
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     public void userLogIn(ActionEvent event) throws IOException {
         checkLogin();
@@ -32,18 +67,16 @@ public class LogInController {
 
     private void checkLogin() throws IOException {
         HelloApplication a = new HelloApplication();
-        if(username.getText().equals("admin") && password.getText().equals("password")) {
+        if(usernameField.getText().equals("admin") && passwordField.getText().equals("password")) {
             errorLabel.setText("Success");
 
             a.changeScene("mainPage.fxml");
         }
-        else if(username.getText().isEmpty() && password.getText().isEmpty()) {
+        else if(usernameField.getText().isEmpty() && passwordField.getText().isEmpty()) {
             errorLabel.setText("Please enter your data");
         }
         else {
             errorLabel.setText("Wrong credentials");
         }
     }
-
-
 }
