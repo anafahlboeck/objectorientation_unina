576e36851293f3b42c9bb671e8b6f7ea0560b75b
AnaFahlböck <ana.fahlboeck@fivesquare.ai>
2024-01-30 19:49:42 +0100

    - added admins to the application - admin is user with boolean is_admin - added TestDataGenerator to the Main

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,45 +1,17 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    user_id
-    SERIAL
-    PRIMARY
-    KEY,
-    email
-    VARCHAR
-(
-    255
-) UNIQUE NOT NULL,
-    password VARCHAR
-(
-    255
-) NOT NULL
+    user_id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    is_admin BOOLEAN DEFAULT false
     );
 
 CREATE TABLE IF NOT EXISTS notes
 (
-    note_id
-    SERIAL
-    PRIMARY
-    KEY,
-    user_id
-    INT,
-    date
-    DATE
-    NOT
-    NULL,
-    header
-    VARCHAR
-(
-    255
-) NOT NULL,
+    note_id SERIAL PRIMARY KEY,
+    user_id INT,
+    date DATE NOT NULL,
+    header VARCHAR(255) NOT NULL,
     text TEXT NOT NULL,
-    CONSTRAINT fk_user
-    FOREIGN KEY
-(
-    user_id
-)
-    REFERENCES users
-(
-    user_id
-)
+    CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(user_id)
     );
